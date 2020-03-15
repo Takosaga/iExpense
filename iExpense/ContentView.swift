8805cadd9c3b4a48f48c18e91cc4481909d70aca
Gonzalo Gamez <takosaga@gmail.com>
2020-03-15 17:13:09 -0500

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Gonzalo Gamez on 3/15/20.
+//  Copyright © 2020 Gamez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
