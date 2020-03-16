87d76a855cd002e0bbdf9546de736bfa9911e441
Gonzalo Gamez <takosaga@gmail.com>
2020-03-16 15:27:00 -0500

    Base Functions without permanent saving

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,45 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddExpense = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+                .sheet(isPresented: $showingAddExpense) {
+                    AddView(expenses: self.expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
